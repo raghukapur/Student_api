36aea2f50b8e1c2c337cbeb85226254bc8dda8c9
raghukapur <raghu.kapur@exotel.in>
2020-01-09 13:28:28 +0530

    the student api

---
+++ student_api.py
@@ -0,0 +1,147 @@
+import flask
+import sqlite3
+from flask import Flask
+from flask import request
+
+
+app = Flask(__name__)
+
+@app.route('/data')
+def get_data():
+    if request.method=="GET":
+        database=sqlite3.connect('student.db')
+        cur=database.cursor()
+        try:
+            cur.execute("SELECT * FROM STUDENT")
+            records=cur.fetchall()
+            print("Total number of students in the table is: ", cur.rowcount)
+            print("\nPrinting each student record")
+            student_details = []
+            for row in records:
+
+                print("SID = ", row[0], )
+                print("FIRSTNAME = ", row[1])
+                print("LASTNAME  = ", row[2])
+                print("PHONE NUMBER  = ", row[3], "\n")
+            database.commit()
+            database.close()
+            return ("THE DATA IS SUCCESSFULLY RETRIEVED")
+        except:
+            database.rollback()
+            database.close()
+            return("CONNECTION ERROR")
+    else:
+        return("REQUEST TYPE ERROR")
+
+
+@app.route('/getspecific')
+def get_specific():
+    if request.method=="GET":
+        database=sqlite3.connect('student.db')
+        cur=database.cursor()
+        print("hello")
+        try:
+            data=request.json
+            if "sid" not in data:
+                return("sid is not entered in the request")
+            cur=database.cursor()
+            cur.execute("SELECT * FROM STUDENT WHERE SID=={}".format(data["sid"]))
+            details=cur.fetchone()
+            if(details==None):
+                print("no corrosponding SID in the table")
+            else:
+                print("SID = ", details[0], )
+                print("FIRSTNAME = ", details[1])
+                print("LASTNAME  = ", details[2])
+                print("PHONE NUMBER  = ", details[3], "\n")
+            database.commit()
+            database.close()
+            return ("The corrosponding details are correctly retrieved from the table")
+        except:
+            database.rollback()
+            database.close()
+            return("failure")
+    else:
+        return("Wrong Request type")
+
+@app.route('/insertdata',methods=['POST'])
+def insert_data():
+    if request.method=="POST":
+        database=sqlite3.connect('student.db')
+        cur=database.cursor()
+        try:
+            data=request.json
+            cur=database.cursor()
+            if "sid" not in data or "lastname" not in data or "firstname" not in data or "phonenumber" not in data:
+                return("All parameters are not passed in the request")
+            try:
+                cur.execute("INSERT INTO STUDENT VALUES({},'{}','{}',{});".format(int(data["sid"]),data["firstname"],data["lastname"],int(data["phonenumber"])))
+            except:
+                print("Data is not in the format")
+                return("error while inserting the entry to the table")
+            database.commit()
+            database.close()
+            return ("success message")
+        except:
+            database.rollback()
+            database.close()
+            return("failure")
+    else:
+        return("Wrong Request type")
+
+
+@app.route('/deletespecific',methods=['DELETE'])
+def delete_specific():
+    if request.method=="DELETE":
+        database=sqlite3.connect('student.db')
+        cur=database.cursor()
+        try:
+            data=request.json
+            cur=database.cursor()
+            cur.execute("DELETE  FROM STUDENT WHERE SID={}".format(data["sid"]))
+            while(True):
+                details=cur.fetchone()
+                if details==None:
+                    break
+                print(details)
+            database.commit()
+            database.close()
+            return ("ENTRY IN DELETED FROM THE TABLE")
+        except:
+            database.rollback()
+            database.close()
+            return("failure")
+    else:
+        return("Wrong Request type")
+
+@app.route('/updatespecific',methods=['PUT'])
+def update_specific():
+    if request.method=="PUT":
+        database=sqlite3.connect('student.db')
+        cur=database.cursor()
+        print("hello")
+        try:
+            data=request.json
+            cur=database.cursor()
+            if "sid" not in data:
+                return("sid is necessary for updates")
+            if "firstname" in data:
+                cur.execute("update STUDENT SET  FIRST_NAME='{}' WHERE SID={};".format(data["firstname"],int(data["sid"])))
+            if "lastname" in data:
+                cur.execute("update STUDENT SET  LAST_NAME='{}'where SID={};".format(data["lastname"],int(data["sid"])))
+            if "phonenumber" in data:
+                cur.execute("update STUDENT SET  PHONE_NUMBER={} WHERE SID={};".format(int(data["phonenumber"]),int(data["sid"])))
+            database.commit()
+            database.close()
+            return ("ENTRY IN UPDATED FROM THE TABLE")
+        except:
+            database.rollback()
+            database.close()
+            return("failure")
+    else:
+        return("Wrong Request type")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
